f01f7672c7780fc074265ac66fa64a24ed067ed4
Exequiel Francia <107739190+Tweakiel@users.noreply.github.com>
2023-02-09 13:17:06 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// TODO: Include packages needed for this application
+//FileSystem
+const fs = require('fs');
+
+//inquirer @ 8.2.4
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
